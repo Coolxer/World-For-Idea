b588f972d6f17d98a1794dc727e30e7fc0c03859
coolxer <lukasik4789@gmail.com>
2020-10-22 10:08:31 +0200

    success configuration

--- src/components/app.js
+++ src/components/app.js
@@ -6,16 +6,9 @@ const App = () => {
     <div className="full-screen">
       <div>
         <h1>
-          React Page {" "}
+          React Page
         </h1>
         <br />
-        <a
-          className="button-line"
-          href="https://github.com/deityhub"
-          target="_blank"
-        >
-          Know more now
-        </a>
       </div>
     </div>
   );
